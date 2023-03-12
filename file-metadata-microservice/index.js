fee938786ce262fc18779c4eecd7216119b14d4d
kristina-sparrow <118679841+kristina-sparrow@users.noreply.github.com>
2023-03-12 12:59:03 -0600

    Finish file metadata microservice

--- file-metadata-microservice/index.js
+++ file-metadata-microservice/index.js
@@ -1,8 +1,9 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
 require('dotenv').config()
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,10 +12,15 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyze', multer().single('upfile'), (req, res) => {
+  res.json({
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+    console.log('Your app is listening on port ' + port)
 });
